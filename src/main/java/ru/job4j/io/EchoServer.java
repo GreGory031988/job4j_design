34e18f5c13e06cf305ad274bc3e20e7d59429105
GreGory031988 <grinrb@mail.ru>
2023-11-09 23:02:25 +0500

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,13 @@
 package ru.job4j.io;
 
+import org.slf4j.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,14 +15,25 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello, dear friend.\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    if (str.contains("msg=Bye")) {
+                    if (str.contains("msg=Exit")) {
                         System.out.println("Завершить работу сервера.");
                         server.close();
+                    } else if (str.contains("msg=Hello")) {
+                        out.write("Hello.".getBytes());
+                        System.out.println("Hello.");
+                    } else {
+                        out.write("What.".getBytes());
+                        System.out.println("What.");
                     }
                     out.flush();
+                } catch (Exception e) {
+                    LOG.error("Exception in log example", e);
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
         }
     }
 }
