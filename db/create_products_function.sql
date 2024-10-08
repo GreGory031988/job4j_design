b6a9b5bb11182d5424700119df7faf81fbd12659
GreGory031988 <grinrb@mail.ru>
2024-10-08 22:32:20 +0500

    3. Хранимые процедуры и функции [#504804]

---
+++ db/create_products_function.sql
@@ -0,0 +1,64 @@
+create
+or replace function f_insert_data(i_name varchar, prod varchar, i_count integer, i_price integer)
+returns void
+language 'plpgsql'
+as
+$$
+    begin
+        insert into products (name, producer, count, price)
+        values (i_name, prod, i_count, i_price);
+    end;
+$$;
+
+select f_insert_data('product_1', 'producer_1', 25, 50);
+
+create
+or replace function f_update_data(u_count integer, tax float, u_id integer)
+returns integer
+language 'plpgsql'
+as
+$$
+    declare
+        result integer;
+    begin
+        if u_count > 0 THEN
+            update products
+            set count = count - u_count
+            where id = u_id;
+            select into result count
+            from products
+            where id = u_id;
+        end if;
+        if tax > 0 THEN
+            update products
+            set price = price + price * tax;
+            select into result sum(price)
+            from products;
+        end if;
+        return result;
+    end;
+$$;
+
+select f_update_data(10, 0, 1);
+
+select f_insert_data('product_2', 'producer_2', 15, 32);
+select f_insert_data('product_3', 'producer_3', 8, 115);
+
+select f_update_data(0, 0.2, 0);
+
+create
+or replace function f_delete_data(d_one_id integer, d_second_id integer)
+returns void
+language 'plpgsql'
+as
+$$
+    begin
+            delete from products
+            where id between d_one_id and d_second_id;
+    end;
+$$;
+
+select f_delete_data (1, 3);
+
+
+
